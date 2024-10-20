ff1932f08062bbb1ead7fa6509bd39d4bcb8aa7d
rarian1995 <A01795124@tec.mx>
2024-10-20 12:56:24 -0700

    Create evaluate_v2.py and refactor plot and evaluate functions

--- pipelines/models/simple_linear_cnn_multi_out_3.py
+++ pipelines/models/simple_linear_cnn_multi_out_3.py
@@ -87,5 +87,78 @@ def train_model(model, dataloader, optimizer, criterion, rmse_metric, epochs=10)
     #    mlflow.log_metric("average_loss", row["average_loss"], step=step)
     #    mlflow.log_metric("rmse", row["rmse"], step=step)
 
+def evaluate_multi_output_metrics(model, test_loader, criterion):
+    model.eval()
 
+    rmse_y1 = torchmetrics.MeanSquaredError(squared=False)
+    rmse_y2 = torchmetrics.MeanSquaredError(squared=False)
+    rmse_y3 = torchmetrics.MeanSquaredError(squared=False)
+
+    test_loss = 0.0
+    total_test_loss = []
+
+    all_y1_true, all_y2_true, all_y3_true = [], [], []
+    all_y1_pred, all_y2_pred, all_y3_pred = [], [], []
+
+    with torch.no_grad():
+        for inputs, y1, y2, y3 in test_loader:
+
+            outputs_y1, outputs_y2, outputs_y3 = model(inputs)
+
+            loss_y1 = criterion(outputs_y1, y1)
+            loss_y2 = criterion(outputs_y2, y2)
+            loss_y3 = criterion(outputs_y3, y3)
+
+            loss = loss_y1 + loss_y2 + loss_y3
+            test_loss += loss.item()
+            total_test_loss.append(loss.item())
+
+            rmse_y1.update(outputs_y1, y1)
+            rmse_y2.update(outputs_y2, y2)
+            rmse_y3.update(outputs_y3, y3)
+
+            all_y1_true.extend(y1.cpu().numpy())
+            all_y2_true.extend(y2.cpu().numpy())
+            all_y3_true.extend(y3.cpu().numpy())
+
+            all_y1_pred.extend(outputs_y1.cpu().numpy())
+            all_y2_pred.extend(outputs_y2.cpu().numpy())
+            all_y3_pred.extend(outputs_y3.cpu().numpy())
+
+    rmse_y1_value = rmse_y1.compute().item()
+    rmse_y2_value = rmse_y2.compute().item()
+    rmse_y3_value = rmse_y3.compute().item()
+
+    # numpy arrays para MAE y R²
+    all_y1_true = np.array(all_y1_true)
+    all_y2_true = np.array(all_y2_true)
+    all_y3_true = np.array(all_y3_true)
+
+    all_y1_pred = np.array(all_y1_pred)
+    all_y2_pred = np.array(all_y2_pred)
+    all_y3_pred = np.array(all_y3_pred)
+
+    # Calculando MAE and R² for each output
+    mae_y1 = mean_absolute_error(all_y1_true, all_y1_pred)
+    mae_y2 = mean_absolute_error(all_y2_true, all_y2_pred)
+    mae_y3 = mean_absolute_error(all_y3_true, all_y3_pred)
+
+    r2_y1 = r2_score(all_y1_true, all_y1_pred)
+    r2_y2 = r2_score(all_y2_true, all_y2_pred)
+    r2_y3 = r2_score(all_y3_true, all_y3_pred)
+
+    results_df = pd.DataFrame(
+        {
+            "Metric": ["RMSE", "MAE", "R²"],
+            "Common Flares (y1)": [rmse_y1_value, mae_y1, r2_y1,y1,outputs_y1],
+            "Moderate Flares (y2)": [rmse_y2_value, mae_y2, r2_y2,y2,outputs_y2],
+            "Severe Flares (y3)": [rmse_y3_value, mae_y3, r2_y3,y3,outputs_y3]
+        }
+    )
+
+    rmse_y1.reset()
+    rmse_y2.reset()
+    rmse_y3.reset()
+
+    return results_df,total_test_loss
 
