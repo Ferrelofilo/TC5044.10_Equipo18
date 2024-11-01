408be60805b8a95a54496681a1c0e413b02f926a
Luis Salomon Flores Ugalde <vf1barukiri@gmail.com>
2024-10-31 20:10:53 -0600

    Train v2 Mlflow integrated

--- pipelines/src/train_v2.py
+++ pipelines/src/train_v2.py
@@ -8,11 +8,12 @@ import pandas as pd
 import torch
 import joblib
 import mlflow
-from tensorboard import summary
 from torch import optim, nn
-
-from pipelines.models.simple_linear_cnn_multi_out_3 import SimpleLinearCnnMO3, train_model
+from torchinfo import summary
+from pipelines.models.simple_linear_cnn_multi_out_3 import SimpleLinearCnnMO3, train_model, ConvolutionalSimpleModel
 from pipelines.utils.data_utils import create_dataloader
+
+
 def load_params():
     with open("params.yaml", 'r') as ymlfile:
         cfg = yaml.safe_load(ymlfile)
@@ -22,20 +23,26 @@ def load_params():
 params = load_params()
 
 
-def create_model(input_len=9):
-    model = SimpleLinearCnnMO3(input_len=input_len)
+# Default settings for Linear CNN and Convolutional CNN
+def create_cnn_model(cnn_type="linear_cnn"):
+    model_params = params['models'][cnn_type]
+    if cnn_type == 'linear_cnn':
+        model = SimpleLinearCnnMO3(**model_params)
+    elif model_type == 'convolutional_cnn':
+        model = ConvolutionalSimpleModel(**model_params)
+
     optimizer = optim.SGD(model.parameters(), lr=0.01)
     criterion = nn.MSELoss()
     rmse_metric = torchmetrics.MeanSquaredError(squared=False)
     return model, optimizer, criterion, rmse_metric
 
 
-def train_model_v2(X_train_path, y_train_path, model_type):
+def train_model_type(X_train_path, y_train_path, model_type):
     x = torch.load(X_train_path)
     y = torch.load(y_train_path)
+    train_loader = create_dataloader(x, y, batch_size=32, shuffle=True)
 
-    train_loader = create_dataloader(x,y,batch_size=32, shuffle=True)
-    model, optimizer, criterion, rmse_metric = create_model(input_len=x.shape[1])
+    model, optimizer, criterion, rmse_metric = create_cnn_model(cnn_type=model_type)
 
     epoch_df = train_model(model, train_loader, optimizer, criterion, rmse_metric, epochs=10)
 
@@ -44,6 +51,18 @@ def train_model_v2(X_train_path, y_train_path, model_type):
         mlflow.log_metric("train_average_loss", row["average_loss"], step=step)
         mlflow.log_metric("train_rmse", row["rmse"], step=step)
 
+    ml_params = {"epochs": 10,
+                 "batch_size": 32,
+                 "shuffle": True,
+                 "optimizer": type(model.optimizer).__name__,
+                 "criterion": type(model.criterion).__name__,
+                 "learning_rate": model.optimizer.param_groups[0]["lr"]}
+    for key, value in optimizer.defaults.items():
+        if f"optimizer_{key}" not in ml_params:
+            ml_params[f"optimizer_{key}"] = value
+
+    mlflow.log_params(ml_params)
+
     return model
 
 
@@ -51,9 +70,9 @@ if __name__ == "__main__":
     X_train_path = sys.argv[1]
     y_train_path = sys.argv[2]
     model_type = sys.argv[3]
-    run_id_out = sys.argv[4]
-    model_dir = params['data']['models']
-    model_path = f"{model_dir}/{model_type}_model.pth"
+    run_id_out = sys.argv[4]  # Json Outfile
+    model_dir = params['data']['models']  # params model location
+    model_path = f"{model_dir}/{model_type}_model.pth"  # model outfile
 
     mlflow.set_experiment(params['mlflow']['experiment_name'])
     mlflow.set_tracking_uri(params['mlflow']['tracking_uri'])
@@ -61,10 +80,10 @@ if __name__ == "__main__":
     with mlflow.start_run() as run:
         run_id = run.info.run_id
         with open(run_id_out, "w") as f:
-            json.dump({"run_id": run_id}, f)
+            json.dump({model_type: run_id}, f)
 
         mlflow.set_tag("phase", "training")
-        model_trained = train_model(X_train_path, y_train_path)
+        model_trained = train_model_type(X_train_path, y_train_path, model_type)
         with open("model_summary.txt", "w") as f:
             f.write(str(summary(model_trained)))
         mlflow.log_artifact("model_summary.txt")
